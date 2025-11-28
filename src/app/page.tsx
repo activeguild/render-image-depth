cbf2b3b6851525a570aa72f0ba593ad201c5a032
j1ngzoue <workdrive0306@gmail.com>
2025-11-28 22:48:25 +0900

    chore: Update types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -105,6 +105,7 @@ export default function Home() {
                 <img src={depthMapSrc} alt="Depth Map" className="max-w-full max-h-full object-contain" />
               ) : (
                 // <div className="text-gray-600 text-[10px]">深度マップ</div>
+                <></>
               )}
             </div>
           </div>
